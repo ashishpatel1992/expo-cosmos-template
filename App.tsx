d58240352a7c2c0733fb851acbdf73468c6c061b
ashish.gpatel <ashish.gpatel@flipkart.com>
2021-04-24 17:19:53 +0530

    expo cosmos integration

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+import React, { Component } from 'react';
+import { NativeFixtureLoader } from 'react-cosmos/native';
+// You generate cosmos.userdeps.js when you start the Cosmos server
+import { rendererConfig, fixtures, decorators } from './cosmos.userdeps';
+
+export default class App extends Component {
+  render() {
+    return (
+      <NativeFixtureLoader
+        rendererConfig={rendererConfig}
+        fixtures={fixtures}
+        decorators={decorators}
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
